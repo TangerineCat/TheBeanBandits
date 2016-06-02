a6ea8182123e1eea97031c49efdd47c64998cc4b
sniffsky <sniffsky@gmail.com>
2016-06-02 02:15:02 -0700

    MAB teaching algorithm added

--- beanbandits/teach.py
+++ beanbandits/teach.py
@@ -144,3 +144,86 @@ class IWSCS_Teach(Teach):
                         self.prev_sample_index + 1) % self.n_samples
                 else:
                     self.revisit_queue.put(self.character_id)
+                    
+                    
+# Multi-armed bandit teaching strategy
+class MAB_Teach(Teach):
+    
+    def __init__(self, n_samples, teach_times):
+        Teach.__init__(self, n_samples, teach_times)
+        self.sample_index = 0
+        self.arm_index = 0
+        
+        self.recent_performance = [0.0 for _ in range(self.n_samples)]
+        self.performance = [0.0 for _ in range(self.n_samples)]
+        self.learning_counts = [0 for _ in range(self.n_samples)]
+        
+        self.indices = range(self.n_samples)
+        
+        self.counts = [0 for _ in range(self.n_samples)]
+        self.avg = [0.0 for _ in range(self.n_samples)]
+        
+    def get_next_teach_sample(self):
+        if not self.terminated():
+            self.teach_process += 1
+            
+            for arm in range(self.n_samples):
+                if self.counts[arm] == 0:
+                    self.arm_index = arm
+                    self.sample_index = self.indices[arm]
+                    return self.sample_index
+            
+            # UCB1 algorithm
+            ucb_values = [0.0 for _ in range(self.n_samples)]
+            total_counts = sum(self.counts)
+            for arm in range(self.n_samples):
+                bonus = math.sqrt((2 * math.log(total_counts)) / float(self.counts[arm]))
+                ucb_values[arm] = self.avg[arm] + bonus
+            
+            self.arm_index = ucb_values.index(max(ucb_values))
+            self.sample_index = self.indices[self.arm_index]
+            return self.sample_index
+
+        else:
+            return (-1)
+        
+    def segment(self):
+        seg_num = int(math.ceil(max(self.performance)))+1
+        seg_list = [[] for _ in range(seg_num)]
+
+        for i in range(self.n_samples):
+            seg_list[int(math.ceil(self.performance[i]))].append(i)
+
+        for i in range(len(seg_list)):
+            random.shuffle(seg_list[i])
+
+        self.indices = []
+        for i in range(len(seg_list)):
+            self.indices += seg_list[i]
+        
+    def teach_judge(self, choice, groundtruth):
+        if not self.terminated():
+            if choice == groundtruth:
+                performance_value = 1.0
+            else:
+                performance_value = 0.0
+                
+            reward = performance_value - self.recent_performance[self.sample_index]
+            # reward = performance_value - self.performance[self.sample_index]
+            
+            self.counts[self.arm_index] += 1
+            n = self.counts[self.arm_index]
+            avg_value = self.avg[self.arm_index]
+            new_avg_value = ((n - 1) / float(n)) * avg_value + (1 / float(n)) * reward
+            self.avg[self.arm_index] = new_avg_value
+            
+            self.performance[self.sample_index] += performance_value
+            self.learning_counts[self.sample_index] += 1
+            n = self.learning_counts[self.sample_index]
+            # self.performance[self.sample_index] = ((n - 1) / float(n)) * self.performance[self.sample_index] + (1 / float(n)) * performance_value
+            if (self.teach_process < self.teach_times):
+                self.indices = sorted(range(len(self.performance)), key=lambda k: self.performance[k])
+            else:
+                self.segment()
+            
+            self.recent_performance[self.sample_index] = performance_value
